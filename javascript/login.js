50e8f5d538010c4c839b5589295f6e73ba4386b7
ThaiLuu92 <matienjp92@gmail.com>
2023-08-27 10:53:52 +0700

    <MD1 - Project>

---
+++ javascript/login.js
@@ -0,0 +1,137 @@
+const container = document.querySelector(".container");
+const passwordShowHiden = document.querySelectorAll(".showHidenEye");
+const passwordFields = document.querySelectorAll(".password");
+
+// JS phần ẩn/hiện mật khẩu
+passwordShowHiden.forEach((eyeIcon) => {
+  eyeIcon.addEventListener("click", () => {
+    passwordFields.forEach((passwordFields) => {
+      if (passwordFields.type === "password") {
+        passwordFields.type = "text";
+        passwordShowHiden.forEach((icon) => {
+          icon.classList.replace("fa-eye-slash", "fa-eye");
+        });
+      } else {
+        passwordFields.type = "password";
+        passwordShowHiden.forEach((icon) => {
+          icon.classList.replace("fa-eye", "fa-eye-slash");
+        });
+      }
+    });
+  });
+});
+
+// JS phần đăng kí
+
+// Tạo database chứa thông tin đăng kí
+
+// B1: lấy thông tin trên các ô input
+const formElement = document.querySelector("form");
+const addUserName = document.getElementById("input-user");
+const addEmail = document.getElementById("input-email");
+const addPassword = document.getElementById("input-password");
+const addRepeatPassword = document.getElementById("input-repeat-password");
+
+
+
+formElement.addEventListener("submit", (e) => {
+  // Chặn action của e
+  e.preventDefault();
+
+  //   B1: Lấy user từ form
+  const user = getUser(); // {user: "", emai: "", password: "", repeatPassword: ""}
+
+  // B2:  Check validator
+  const error = checkError(user); // { } --> chứa thông tin lỗi
+  renderError(error); // hiển thị loại
+ 
+  if (error.isError) {
+    return;
+  }
+
+  // B3:  Kiểm tra email có tồn tai trên database
+  const accountsDB = JSON.parse(localStorage.getItem("accounts")) || [];
+
+  let isExist = false;
+  //for
+  for (const account of accountsDB) {
+    if (account.email === user.email) {
+      isExist = true;
+      break;
+    }
+  }
+
+  //   B4: Điều hướng login
+  if (!isExist) {
+    // Ok --> đẩy lên local
+    delete user.repeatPassword;
+    accountsDB.push(user);
+
+    localStorage.setItem("accounts", JSON.stringify(accountsDB));
+    // Điều hướng login
+  } else {
+    error.isError = true;
+    error.msgEmail =
+      "Email đã tồn tại, vui lòng đăng nhập hoặc đăng ký email khác";
+    console.log(1111, error);
+    renderError(error);
+  }
+});
+
+
+
+// lấy thông tin người dùng
+
+function getUser() {
+  return {
+    userName: addUserName.value,
+    email: addEmail.value.toLowerCase().trim(),
+    password: addPassword.value,
+    repeatPassword: addRepeatPassword.value,
+  };
+}
+function checkError(user) {
+  const error = {
+    isError: false,
+    msgUserName: "",
+    msgEmail: "",
+    msgPassword: "",
+    msgRepeatPassword: "",
+  };
+
+  const validRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if (!user.userName) {
+    error.isError = true;
+    error.msgUserName = "Tên tài khoản không được để trống";
+  }
+  if (user.password !== user.repeatPassword) {
+    error.isError = true;
+    error.msgRepeatPassword =
+      "Mật khẩu không trùng khớp, vui lòng nhập lại";
+  }
+  if (!user.password) {
+    error.isError = true;
+    error.msgPassword = "Mật khẩu không được để trống";
+  }
+
+  if (!user.email.match(validRegex)) {
+    error.isError = true;
+    error.msgEmail = "Email không đúng định dạng";
+  }
+
+  return error;
+}
+
+function renderError(error) {
+  const errorUserNameElement = document.querySelector("#error-user");
+  const errorEmailElement = document.querySelector("#error-email");
+
+  const errorPasswordElement = document.querySelector("#error-password");
+  const errorRepeatPasswordElement = document.querySelector(
+    "#error-repeat-password"
+  );
+  errorUserNameElement.textContent = error.msgUserName
+  errorEmailElement.textContent = error.msgEmail;
+  errorPasswordElement.textContent = error.msgPassword;
+  errorRepeatPasswordElement.textContent = error.msgRepeatPassword;
+}
