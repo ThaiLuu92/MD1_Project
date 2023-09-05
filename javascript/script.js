77510172b47aa60cfd2153e2259393180bc337b3
ThaiLuu92 <matienjp92@gmail.com>
2023-09-05 15:22:02 +0700

    update

---
+++ javascript/script.js
@@ -0,0 +1,15 @@
+// var swiper = new Swiper(".slide-content", {
+//   slidesPerView: 3,
+//   spaceBetween: 30,
+//   slidesPerGroup: 3,
+//   loop: true,
+//   loopFillGroupWithBlank: true,
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+//   navigator: {
+//     nextEL: ".swiper-button-next",
+//     prevEL: ".swiper-button-prev",
+//   },
+// });
